56888a253365c0d76bbfac37426b5b01ad6aff7b
nds156 <a.a.t.soul@hotmail.com>
2018-01-18 15:07:59 +0200

    Add project files.

---
+++ DataNodes/HtmlPage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace DataNodes
+{
+    public class HtmlPage
+    {
+        public IEnumerable<string> Parser(string url = "http://www.pravda.com.ua/"
+                                            , string xPath = "//div[@class='news tab_item']"
+                                            ,string codepageName = "windows-1251")
+        {
+            string htmlContent = "";
+
+            using (WebClient client = new WebClient())
+            {
+                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+                using (Stream data = client.OpenRead(new Uri(url)))
+                using (StreamReader reader = new StreamReader(data, Encoding.GetEncoding(codepageName)))
+                {
+                    htmlContent = reader.ReadToEnd();
+                }
+            }
+
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+
+            doc.LoadHtml(htmlContent);
+
+            var x = doc.DocumentNode.SelectNodes(xPath);
+            foreach (var item in x)
+            {
+                yield return item.InnerHtml;
+            }
+        }
+    }
+}
---
+++ HotNews/Controllers/PravdaComUaController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DataNodes;
+using System.Net;
+using System.Web;
+
+namespace HotNews.Controllers
+{
+    [Route("api/[controller]")]
+    public class PravdaComUaController : Controller
+    {
+        // GET api/PravdaComUa
+        [HttpGet]
+        public ContentResult Get()
+        {
+            HtmlPage html = new HtmlPage();
+            var value = html.Parser();
+            string htmlCode = "";
+            foreach (var item in html.Parser())
+            {
+                htmlCode += HttpUtility.HtmlDecode(item);
+            }
+
+            return new ContentResult
+            {
+                ContentType = "text/html;charset=windows-1251",
+                StatusCode = (int)HttpStatusCode.OK,
+                Content = "<!DOCTYPE html> <html><body>" + htmlCode + "</body></html>"
+            };
+        }
+    }
+}
